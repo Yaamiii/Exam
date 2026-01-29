ebb44b8f5fed49d39729a2ef109977cbb5eb180c
Valentina <52736165+Yaamiii@users.noreply.github.com>
2026-01-29 10:41:40 +0100

    Add horizontal movement limits to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 3f;
-    public float horizontalInput;
+    private float minX = -1.5f;
+    private float maxX = 1.5f;
+
+    private float horizontalInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,5 +24,24 @@ public class PlayerController : MonoBehaviour
         // Move left/right
 
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        // Keep the player inside the horizontal bounds
+        if (transform.position.x < minX)
+        {
+            transform.position = new Vector3(
+                minX,
+                transform.position.y,
+                transform.position.z
+            );
+        }
+
+        if (transform.position.x > maxX)
+        {
+            transform.position = new Vector3(
+                maxX,
+                transform.position.y,
+                transform.position.z
+            );
+        }
     }
 }
